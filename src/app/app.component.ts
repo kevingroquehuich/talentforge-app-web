73a86eb43d82f4f56ddc466946c8042421370c39
Keving Hanz Roque Huich <keving.roque.huich@gmail.com>
2024-06-24 01:43:29 -0500

    Se agrega chatbor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,7 +9,8 @@ import { AuthenticationComponent } from './components/authentication/authenticat
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent, AuthenticationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'talentforge-app';
