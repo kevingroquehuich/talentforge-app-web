2f531a0f4eed9765dcee751d1f9f9c6d9189f6f6
Keving Hanz Roque Huich <keving.roque.huich@gmail.com>
2024-06-25 16:56:57 -0500

    Se implementa contactos y se cambia de diseño servicios

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
+import { MapsComponent } from '../../components/maps/maps.component';
+import { BreadcrumbsComponent } from '../../components/breadcrumbs/breadcrumbs.component';
+import { LottieAnimationComponent } from '../../components/lottie-animation/lottie-animation.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent, MapsComponent, BreadcrumbsComponent, LottieAnimationComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
