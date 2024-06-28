2a3bc129186fe6e3e6a5224595b23394c8e51bfe
Keving Hanz Roque Huich <keving.roque.huich@gmail.com>
2024-06-27 22:33:16 -0500

    Se implenta formularios y se obtienen respuestas

--- src/app/components/question-survey/question-survey.component.ts
+++ src/app/components/question-survey/question-survey.component.ts
@@ -1,12 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-question-survey',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './question-survey.component.html',
   styleUrl: './question-survey.component.scss'
 })
 export class QuestionSurveyComponent {
   @Input() question: any;
+
+  surveyForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+    this.surveyForm = this.fb.group({});
+  }
 }
--- src/app/pages/surveys/form-survey/form-survey.component.ts
+++ src/app/pages/surveys/form-survey/form-survey.component.ts
@@ -3,39 +3,47 @@ import { BreadcrumbsComponent } from '../../../components/breadcrumbs/breadcrumb
 import { ActivatedRoute } from '@angular/router';
 import { QuestionSurveyComponent } from '../../../components/question-survey/question-survey.component';
 import { SurveyService } from '../../../services/survey.service';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-survey',
   standalone: true,
-  imports: [BreadcrumbsComponent, QuestionSurveyComponent],
+  imports: [BreadcrumbsComponent, QuestionSurveyComponent, ReactiveFormsModule],
   templateUrl: './form-survey.component.html',
   styleUrl: './form-survey.component.scss'
 })
 export default class FormSurveyComponent implements OnInit{
 
+  surveyForm: FormGroup = new FormGroup({});
   survey: any;
   questions: any[] = [];
 
-  constructor(private route: ActivatedRoute, private dataService: SurveyService) {}
+
+  constructor(
+    private route: ActivatedRoute, 
+    private dataService: SurveyService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+
+    this.surveyForm = this.fb.group({});
+
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.dataService.getSurveyById(id).subscribe(
         (survey) => {
           this.survey = survey;
-          console.log('survey', survey)
 
           this.dataService.getQuestions(id).subscribe(
             (questions) => {
               this.questions = questions;
-              console.log('questions', questions)
+              this.buildForm();
 
               this.questions.forEach(q => {
                 this.dataService.getQuestionOptions(id, q.id).subscribe (
                   (options) => {
                     q.options = options;
-                    console.log(`Options for question ${q.id}:`, options);
                   },
                   error => console.error('Error fetching options:', error)
                 )
@@ -45,24 +53,56 @@ export default class FormSurveyComponent implements OnInit{
 
         },
         (error) => console.error('Error fetching survey:', error)
-
-        /*questions => {
-          console.log('Questions:', questions);
-          this.questions = questions;
-          this.questions.forEach(question => {
-            this.dataService.getQuestionOptions(id, question.id).subscribe(
-              options => {
-                console.log(`Options for question ${question.id}:`, options); // Añadir log
-                question.options = options;
-              },
-              error => console.error('Error fetching options:', error) // Añadir log
-            );
-          });
-        },
-        error => console.error('Error fetching survey:', error) // Añadir log */
       ); 
+    } 
+  }
+
+
+  buildForm() {
+    this.questions.forEach(question => {
+      this.surveyForm.addControl(question.id, this.fb.control(null));
+    });
+  }
+
+  onSubmit() {
+
+    const formData = this.surveyForm.value;
+    console.log('Respuestas', formData)
+
+
+    if (this.surveyForm.valid) {
+      const formData = this.surveyForm.value;
+
+      // Mapear el formulario a un formato que Firestore espera
+      const dataToSave = {
+        name: formData.name,
+        email: formData.email,
+        age: formData.age,
+        gender: formData.gender,
+        jobCategory: formData.jobCategory,
+        questions: formData.questions.map((question: any) => ({
+          id: question.id,
+          selectedOption: question.selectedOption
+        }))
+      };
+      console.log('Respuestas-dentro', formData)
+      console.log('DataToSave-dentro', dataToSave)
+
+      
+      // Guardar las respuestas en Firestore usando el servicio PreguntasService
+      /*this.preguntasService.guardarRespuestas(respuestas).then(
+        () => {
+          console.log('Respuestas guardadas correctamente en Firestore.');
+          // Puedes resetear el formulario aquí si lo deseas
+          this.preguntasForm.reset();
+        },
+        (error) => {
+          console.error('Error al guardar las respuestas:', error);
+        }
+      ); */
+    } else {
+      console.error('Formulario no válido. Revise los campos.');
     }
-    
   }
 
 }
