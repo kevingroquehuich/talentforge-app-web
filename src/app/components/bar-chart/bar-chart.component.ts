9331b7edf45122a615bf6b83e3503a3b55d7f37f
Keving Hanz Roque Huich <keving.roque.huich@gmail.com>
2024-07-01 12:48:18 -0500

    Se agregan graficos

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SurveyQuestion, UserSurveyResponseData } from '../../models/user-survey-response-data.model';
+import { ChartType } from 'chart.js';
+import { MatCardModule } from '@angular/material/card';
+import { BaseChartDirective } from 'ng2-charts';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-bar-chart',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, BaseChartDirective],
   templateUrl: './bar-chart.component.html',
   styleUrl: './bar-chart.component.scss'
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnInit {
+
+  @Input() surveyId: string = '';
+
+  surveyResponses: UserSurveyResponseData[] = [];
+
+
+  constructor(private dataService: DataService) { }
+
+
+  ngOnInit(): void {
+    this.dataService.getResponsesSurvey(this.surveyId).subscribe(data => {
+      this.barChartData = this.processDataResponses(data);;
+      this.surveyResponses = data;
+    });
+  }
+
+  public barChartOptions = {
+    responsive: true,
+    scales: {
+      xAxes: [{ stacked: true }],
+      yAxes: [{ stacked: true }]
+    }
+  };
+  public barChartLabels = [
+    'Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4', 'Pregunta 5',
+    'Pregunta 6', 'Pregunta 7', 'Pregunta 8', 'Pregunta 9', 'Pregunta 10',
+    'Pregunta 11', 'Pregunta 12', 'Pregunta 13', 'Pregunta 14', 'Pregunta 15',
+    'Pregunta 16', 'Pregunta 17', 'Pregunta 18', 'Pregunta 19', 'Pregunta 20',
+
+  ];
+
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData: any[] = [];
+  public barChartPlugins = [];
+
+  private processDataResponses(responses: UserSurveyResponseData[]): any[] {
+    const aggregatedData = Array(20).fill(null).map(() => ({
+      'Totalmente en desacuerdo': 0,
+      'En desacuerdo': 0,
+      'Ni de acuerdo ni en desacuerdo': 0,
+      'De acuerdo': 0,
+      'Totalmente de acuerdo': 0,
+    }));
+
+    responses.forEach(response => {
+      response.questions.forEach((question: SurveyQuestion, index: number) => {
+        const answer = question.selectedOption.name as keyof typeof aggregatedData[0];
+        if (aggregatedData[index][answer] !== undefined) {
+          aggregatedData[index][answer]++;
+        }
+      });
+    });
+
+    return [
+      { data: aggregatedData.map(item => item['Totalmente en desacuerdo']), label: 'Totalmente en desacuerdo' },
+      { data: aggregatedData.map(item => item['En desacuerdo']), label: 'En desacuerdo' },
+      { data: aggregatedData.map(item => item['Ni de acuerdo ni en desacuerdo']), label: 'Ni de acuerdo ni en desacuerdo' },
+      { data: aggregatedData.map(item => item['De acuerdo']), label: 'De acuerdo' },
+      { data: aggregatedData.map(item => item['Totalmente de acuerdo']), label: 'Totalmente de acuerdo' },
+    ];
+  }
 
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,140 +7,24 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { BreadcrumbsComponent } from '../../components/breadcrumbs/breadcrumbs.component';
-import { DataService } from '../../services/data.service';
 import { BaseChartDirective } from 'ng2-charts';
-import { ChartConfiguration, ChartData, ChartDataset, ChartOptions, ChartType } from "chart.js";
-import { SurveyQuestion, UserSurveyResponseData } from '../../models/user-survey-response-data.model';
 import { FormsModule } from '@angular/forms';
 import { TableUsersComponent } from '../../components/table-users/table-users.component';
 import { PieChartComponent } from '../../components/pie-chart/pie-chart.component';
+import { BarChartComponent } from '../../components/bar-chart/bar-chart.component';
 
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FormsModule, BreadcrumbsComponent, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatIconModule, BaseChartDirective, MatCardModule, TableUsersComponent, PieChartComponent],
+  imports: [FormsModule, BreadcrumbsComponent, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatIconModule, BaseChartDirective, MatCardModule, TableUsersComponent, PieChartComponent, BarChartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export default class DashboardComponent implements AfterViewInit {
+export default class DashboardComponent{
 
 
-  constructor(private dataService: DataService) { }
-
-  ngAfterViewInit(): void {
-    this.fetchAllSurveyResponses();
-  }
-
-
-
-  fetchAllSurveyResponses() {
-    this.dataService.getResponsesSurvey('lIATWyLizRdCXZxOCbjY').subscribe(data => {
-      this.prepareChartData(data);
-      this.barChartData = this.processDataResponses(data);;
-    });
-
-    this.dataService.getResponsesSurvey('PpXbEMSDTnSaaryZnl3y').subscribe(data => {
-
-    });
-
-    this.dataService.getResponsesSurvey('M6JRYSj3qK4fF35y7U2Q').subscribe(data => { });
-
-
-    this.dataService.getResponsesSurvey('QfkopBkUkpDAvKxTOd7J').subscribe(data => {
-    });
-  }
-
-
-  /*========================================== */
-  /*===================CHARTS================= */
-  /*========================================== */
-  public barChartOptions = {
-    responsive: true,
-    scales: {
-      xAxes: [{ stacked: true }],
-      yAxes: [{ stacked: true }]
-    }
-  };
-  public barChartLabels = [
-    'Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4', 'Pregunta 5',
-    'Pregunta 6', 'Pregunta 7', 'Pregunta 8', 'Pregunta 9', 'Pregunta 10',
-    'Pregunta 11', 'Pregunta 12', 'Pregunta 13', 'Pregunta 14', 'Pregunta 15',
-    'Pregunta 16', 'Pregunta 17', 'Pregunta 18', 'Pregunta 19', 'Pregunta 20',
-
-  ];
-
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
-  public barChartData: any[] = [];
-  public barChartPlugins = [];
-
-
-  private processDataResponses(responses: UserSurveyResponseData[]): any[] {
-    const aggregatedData = Array(20).fill(null).map(() => ({
-      'Totalmente en desacuerdo': 0,
-      'En desacuerdo': 0,
-      'Ni de acuerdo ni en desacuerdo': 0,
-      'De acuerdo': 0,
-      'Totalmente de acuerdo': 0,
-    }));
-
-    responses.forEach(response => {
-      response.questions.forEach((question: SurveyQuestion, index: number) => {
-        const answer = question.selectedOption.name as keyof typeof aggregatedData[0];
-        if (aggregatedData[index][answer] !== undefined) {
-          aggregatedData[index][answer]++;
-        }
-      });
-    });
-
-    return [
-      { data: aggregatedData.map(item => item['Totalmente en desacuerdo']), label: 'Totalmente en desacuerdo' },
-      { data: aggregatedData.map(item => item['En desacuerdo']), label: 'En desacuerdo' },
-      { data: aggregatedData.map(item => item['Ni de acuerdo ni en desacuerdo']), label: 'Ni de acuerdo ni en desacuerdo' },
-      { data: aggregatedData.map(item => item['De acuerdo']), label: 'De acuerdo' },
-      { data: aggregatedData.map(item => item['Totalmente de acuerdo']), label: 'Totalmente de acuerdo' },
-    ];
-  }
-
-
-
-  public pieChartOptions: ChartConfiguration['options'] = {
-    plugins: {
-      legend: {
-        display: true,
-        position: 'right',
-        fullSize: true
-      }
-    },
-  };
-
-  public pieChartType: ChartType = 'pie';
-  public pieChartData!: ChartData<'pie', number[], string | string[]>;
-
-  prepareChartData(surveyResponses: UserSurveyResponseData[]) {
-    const counts: Record<string, number> = {};
-    surveyResponses.forEach(response => {
-      response.questions.forEach(question => {
-        const optionName = question.selectedOption.name;
-        if (counts[optionName]) {
-          counts[optionName]++;
-        } else {
-          counts[optionName] = 1;
-        }
-      });
-    });
-
-    this.pieChartData = {
-      labels: Object.keys(counts),
-      datasets: [
-        {
-          data: Object.values(counts)
-        }
-      ]
-    }
-  }
 
 }
 
